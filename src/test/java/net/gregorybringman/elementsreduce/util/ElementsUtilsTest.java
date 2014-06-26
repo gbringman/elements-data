ac7ac875f6ddbc8b373f40be18135a860cfb6bf1
Gregory Bringman <brin0126@umn.edu>
2014-06-26 09:45:01 -0500

    Added mini-specs to each test and added new test case.

--- src/test/java/net/gregorybringman/elementsreduce/util/ElementsUtilsTest.java
+++ src/test/java/net/gregorybringman/elementsreduce/util/ElementsUtilsTest.java
@@ -55,6 +55,10 @@ public class ElementsUtilsTest {
         assertEquals("Created Range is incorrect", expected, actual);
     }
 
+    /*
+     * Test that the invocation of a line range match test inside {@link this#fetchRange} 
+     * will fail and that the range values will match their initialization value, 0.
+     */
     @Test
     public void fetchRangeNoPOSMatch() {
 
@@ -66,6 +70,11 @@ public class ElementsUtilsTest {
         assertEquals("Created Range is incorrect", expected, actual);
     }
 
+    /*
+     * Validate that the {@link this#fetchPage} method recognizes page 
+     * numbers in the data and creates an equivalent {@link IntWritable} 
+     * for each.
+     */
     @Test
     public void fetchPage() {
         
@@ -75,6 +84,10 @@ public class ElementsUtilsTest {
         Assert.assertEquals(expected, actual);
     }
 
+    /*
+     * Test that the invocation of a page match test inside {@link this#fetchPage} 
+     * will fail and that it then matches its initialization value, 0.
+     */
     @Test
     public void fetchPageNoPOSMatch() {
         
@@ -86,8 +99,17 @@ public class ElementsUtilsTest {
         Assert.assertEquals(expected, actual);
     }
 
+    /*
+     * Validate that the selected page is the integer value that is the 
+     * right operand of the French lexeme for &quot;to&quot;, &quot;à&quot;.
+     * 
+     * TODO://evaluate whether this is the desired behavior.
+     */
     @Test
     public void fetchPageWithToText() {
+        
+        // &quot;To text&quot;, meaning a page range is specified with the French
+        // lexeme for &quot;to&quot;, an &quot;a&quot; with a grave accent.
         
         IntWritable actual = ElementsUtils.fetchPage("30 à 397");
         IntWritable expected = new IntWritable(397);
@@ -151,6 +173,11 @@ public class ElementsUtilsTest {
             ElementsUtils.markupPages(entry, "A\nB\nC\nD\nE\nF\nG\nH"));
     }
     
+    /*
+     * Test that an instance of {@link ElementsMapWritable} is created, with, 
+     * for only the current map or reduce phase, a properly serialized POS 
+     * key.
+     */
     @Test
     public void populateEntry() {
         ElementsStringArrayWritable range = mock(ElementsStringArrayWritable.class);
@@ -164,5 +191,27 @@ public class ElementsUtilsTest {
         ElementsUtils.populateEntry(emw, expectedRanges);
         
         verify(emw).put(new Text("pagewlinerng_1"), ElementsUtils.fetchRange(stringRanges[1]));
+    }
+    
+    /*
+     * Test the condition when incoming data has two instances of the same POS, that 
+     * its serial suffix will then disambiguate inside the map structure, i.e.: 
+     * &quot;pagewlinerng_1&quot; and &quot;pagewlinerng_4&quot; will be distinct 
+     * keys.
+     */
+    @Test
+    public void populateEntryWSerializedKeys() {
+        ElementsStringArrayWritable range = mock(ElementsStringArrayWritable.class);
+        Writable[] expectedRanges = new Writable[] { range, range };
+
+        ElementsMapWritable emw = mock(ElementsMapWritable.class);
+        String [] stringRanges = new String[] { "", "1, 4-6", "2, 5-7" };
+        
+        when(range.toStrings()).thenReturn(stringRanges);
+        
+        ElementsUtils.populateEntry(emw, expectedRanges);
+        
+        verify(emw).put(new Text("pagewlinerng_1"), ElementsUtils.fetchRange(stringRanges[1]));
+        verify(emw).put(new Text("pagewlinerng_2"), ElementsUtils.fetchRange(stringRanges[2]));
     }
 }
